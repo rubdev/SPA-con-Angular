5afe6324c5fc4bf266162fc7c81cfd9218c1a528
Rubén Segura <rsegrom@gmail.com>
2019-07-06 20:03:37 +0200

    generados primeros componentes y puesta navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AboutComponent,
+    HeroesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule

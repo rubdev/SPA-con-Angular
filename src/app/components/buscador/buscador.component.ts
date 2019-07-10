c77759cb25afb82fd95383d43e61dc715022abf7
Rubén Segura <rsegrom@gmail.com>
2019-07-10 11:50:38 +0200

    Recoge termino de búsqueda y la realiza buscando resultados

--- src/app/components/buscador/buscador.component.ts
+++ src/app/components/buscador/buscador.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService, Heroe } from '../../services/heroes.service';
+
+
 
 @Component({
   selector: 'app-buscador',
@@ -7,9 +11,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuscadorComponent implements OnInit {
 
-  constructor() { }
+  heroes: Heroe[] = [];
+
+  constructor( private activatedRoute: ActivatedRoute, private _heroesService: HeroesService ) { 
+
+  }
 
   ngOnInit() {
+
+    this.activatedRoute.params.subscribe( params => {
+      this.heroes = this._heroesService.buscarHeroes( params['busqueda'] );
+      console.log(this.heroes);
+    });
+
   }
 
 }

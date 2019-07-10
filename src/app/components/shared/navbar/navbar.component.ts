7083a77b964a80d09786bb7205b6689d2f88ef02
Rubén Segura <rsegrom@gmail.com>
2019-07-10 11:50:04 +0200

    Añadida ruta al componente buscador con término de búsqueda

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-navbar',
@@ -7,13 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor( private router: Router ) { }
 
   ngOnInit() {
   }
 
   buscarHeroe( busqueda: string ) {
     console.log(busqueda);
+    if (busqueda.length > 0) {
+      this.router.navigate(['/buscar', busqueda]);
+    }
   }
 
 }

e11e48d079ef7e28d06aa2bd7fb97ece02140da4
Rubén Segura <rsegrom@gmail.com>
2019-07-10 11:32:18 +0200

    Agregada ruta para el buscador

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroeComponent } from './components/heroe/heroe.component';
+import { BuscadorComponent } from './components/buscador/buscador.component';
 
 
 const ROUTES: Routes = [
@@ -10,6 +11,7 @@ const ROUTES: Routes = [
     { path: 'about', component: AboutComponent },
     { path: 'heroes', component: HeroesComponent },
     { path: 'heroe/:id', component: HeroeComponent },
+    { path: 'buscar/:termino', component: BuscadorComponent },
     { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
